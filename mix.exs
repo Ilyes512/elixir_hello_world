60b37eccdc0dd3a24555982b650ef6aeb223a522
Ilyes512 <ilyes.ahidar@gmail.com>
2017-11-25 23:15:34 +0100

    Added cowboy erlang package

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule HelloWorld.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:cowboy, "~> 2.1"}
     ]
   end
 end
