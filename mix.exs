76b161536ff9e1afcbea0e83089584d95cbc3a80
Ilyes512 <ilyes.ahidar@gmail.com>
2017-11-26 23:30:44 +0100

    Added Distillery

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule HelloWorld.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:cowboy, "~> 2.1"}
+      {:cowboy, "~> 2.1"},
+      {:distillery, "~> 1.5"}
     ]
   end
 end
